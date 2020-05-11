5b86cdc5733f520653587b1f0209387c2866ed98
Isaac Sokari <isaacsokari@gmail.com>
2020-05-11 17:30:52 +0100

    drawn initial canvas items

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -1,10 +1,65 @@
 const rulesBtn = document.getElementById("rules-btn");
 const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+// initial score
+let score = 0;
 
+// create ball properties
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
 
+// create paddle properties
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
 
+// draw ball
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// draw paddle
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// draw score
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+// draw all items
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+
+draw();
+f;
 // show and hide rules
 rulesBtn.addEventListener("click", () => {
   rules.classList.add("show");
