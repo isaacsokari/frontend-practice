490fe90c3ba9948c641ccd22374bbd93d6209741
Isaac Sokari <isaacsokari@gmail.com>
2020-05-11 21:25:46 +0100

    inserted items into DOM, completed styling, noFunc

---
+++ sortable-list/script.js
@@ -0,0 +1,46 @@
+const draggableList = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
+
+// store listItems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// insert list items into DOM
+function createList() {
+  [...richestPeople]
+    .map((text) => ({ value: text, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((object) => object.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+      <span class="number">${index + 1}.</span>
+      <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines"></i>
+      </div>
+    `;
+
+      listItems.push(listItem);
+
+      draggableList.appendChild(listItem);
+    });
+}
